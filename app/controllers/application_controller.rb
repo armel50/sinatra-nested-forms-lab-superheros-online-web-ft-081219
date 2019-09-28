8cd69b380fc3958e5b06e2d3569ecb8d8dddce6c
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-28 05:59:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
+require_relative '../models/team.rb'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
+  get '/' do 
+    erb :super_hero
+  end 
+  
+  post '/' do
+    # p params
+    @team = Team.new(team_name: params["team"]["name"], motto:  params["team"]["motto"], hereos:  params["team"]["heroes"])
+    P @team.team_name
+    erb :team
+  end
 
 end
